fee09b44c6295541599e3c5670937342c5b9ddf1
Selvi Selvaraj <selviselvaraj2112@gmail.com>
2026-08-30 11:24:19 +0530

    Add JWT token creation

--- app/auth.py
+++ app/auth.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta, timezone
+from jose import jwt
 import bcrypt
 
 
@@ -11,3 +13,15 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     plain_bytes = plain_password.encode("utf-8")[:72]
     hashed_bytes = hashed_password.encode("utf-8")
     return bcrypt.checkpw(plain_bytes, hashed_bytes)
+
+
+SECRET_KEY = "change-this-to-something-random-later"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
